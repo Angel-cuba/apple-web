6941d22a9c83987c4e66b9db0f51ac254216dd75
Angel <araozangel842@gmail.com>
2025-10-23 14:56:26 +0100

    Implement Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section id="hero">
+      <div>
+        <h1>Mac Book Pro 16"</h1>
+        <img src="/title.png" alt="Mac Book Pro 16 inch" />
+      </div>
+      <video src="/videos/hero.mp4" autoPlay muted playsInline />
+      <button>Buy</button>
+      <p>From $1499 or $130/mo for 12 months</p>
+    </section>
+  );
+}
