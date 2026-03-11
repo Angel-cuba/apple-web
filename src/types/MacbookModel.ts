7d903755c0232216dfe62a09af9ff56a2abb70ac
Angel <araozangel842@gmail.com>
2026-03-11 09:45:09 +0000

    Agregar modelos de Macbook y tipos GLTF correspondientes

---
+++ src/types/MacbookModel.ts
@@ -0,0 +1,48 @@
+// src/types/MacbookModel16.ts
+import type { GLTF } from "three-stdlib";
+import type { Mesh, Material, BufferGeometry } from "three";
+
+export type MacbookGLTFResult = GLTF & {
+  nodes: {
+    Object_10: Mesh<BufferGeometry, Material>;
+    Object_16: Mesh<BufferGeometry, Material>;
+    Object_20: Mesh<BufferGeometry, Material>;
+    Object_22: Mesh<BufferGeometry, Material>;
+    Object_30: Mesh<BufferGeometry, Material>;
+    Object_32: Mesh<BufferGeometry, Material>;
+    Object_34: Mesh<BufferGeometry, Material>;
+    Object_38: Mesh<BufferGeometry, Material>;
+    Object_42: Mesh<BufferGeometry, Material>;
+    Object_48: Mesh<BufferGeometry, Material>;
+    Object_54: Mesh<BufferGeometry, Material>;
+    Object_58: Mesh<BufferGeometry, Material>;
+    Object_66: Mesh<BufferGeometry, Material>;
+    Object_74: Mesh<BufferGeometry, Material>;
+    Object_82: Mesh<BufferGeometry, Material>;
+    Object_96: Mesh<BufferGeometry, Material>;
+    Object_107: Mesh<BufferGeometry, Material>;
+    Object_123: Mesh<BufferGeometry, Material>;
+    Object_127: Mesh<BufferGeometry, Material>;
+  };
+  materials: {
+    PaletteMaterial001: Material;
+    zhGRTuGrQoJflBD: Material;
+    PaletteMaterial002: Material;
+    lmWQsEjxpsebDlK: Material;
+    LtEafgAVRolQqRw: Material;
+    iyDJFXmHelnMTbD: Material;
+    eJObPwhgFzvfaoZ: Material;
+    nDsMUuDKliqGFdU: Material;
+    CRQixVLpahJzhJc: Material;
+    YYwBgwvcyZVOOAA: Material;
+    SLGkCohDDelqXBu: Material;
+    WnHKXHhScfUbJQi: Material;
+    fNHiBfcxHUJCahl: Material;
+    LpqXZqhaGCeSzdu: Material;
+    gMtYExgrEUqPfln: Material;
+    PaletteMaterial003: Material;
+    JvMFZolVCdpPqjj: Material;
+    sfCQkHOWyrsLmor: Material;
+    ZCDwChwkbBfITSW: Material;
+  };
+};
